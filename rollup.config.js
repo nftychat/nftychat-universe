34f17632d7f3cdcb3c1b8733a0a011458f2fc825
Darian Bailey <darian@nftychat.xyz>
2022-09-27 17:07:26 -0400

    checkpoint

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,6 @@ import styles from "rollup-plugin-styles";
 import commonjs from '@rollup/plugin-commonjs';
 import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
 
-
 export default {
     input: pkg.source,
     output: [
@@ -28,6 +27,7 @@ export default {
         //commonjs used by rainbowkit
         resolve({preferBuiltins: true}),
         commonjs({
+            transformMixedEsModules:true,
             include: /node_modules/
         }),
         //dynamicImportVars used by rainbowkit
